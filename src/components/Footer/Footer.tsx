294c98b406cd369ebcc3bedaac8d39dda9ddadde
TheoRomanova <feodoraandriewskaya@gmail.com>
2023-06-01 15:41:27 +0300

    m

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,46 @@
 import "./styles.scss";
 export const Footer = () => {
-  return <footer className="app-footer">FOOTER</footer>;
+  return (
+    <footer className="app-footer">
+      <div className="contacts-info">
+        <div className="logo"></div>
+        <div className="contacts">
+          <div className="social-media">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="tel-mail">
+            <p>{"  "} +7 951 999 28 34</p>
+            <p>{"  "} info@stageboxbrand.ru</p>
+          </div>
+        </div>
+      </div>
+      <div className="shop-info">
+        <div className="info-block">
+          <p>Доставка</p>
+          <p>Гарантии</p>
+          <p>Таблица размеров</p>
+          <p>
+            <b>Обмен и возврат</b>
+          </p>
+          <p>Вопросы и ответы</p>
+        </div>
+        <div className="profile">
+          <ul className="profile-icons">
+            <li>
+              <span></span>
+            </li>
+            <li>
+              <span></span>
+            </li>
+          </ul>
+          <div className="search">
+            <input placeholder="Поиск"></input>
+            <button>поиск</button>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
