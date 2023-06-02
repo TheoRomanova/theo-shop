4a7679ee1cdc02e91464991a98cd856cabf2f2b4
TheoRomanova <feodoraandriewskaya@gmail.com>
2023-06-02 12:24:58 +0300

    g

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { MainPage } from "./pages/Main/Main";
@@ -7,9 +8,11 @@ export const App = () => {
   return (
     <div className="app">
       <Header />
-      {/* router */}
-      <MainPage />
-      {/* router */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
