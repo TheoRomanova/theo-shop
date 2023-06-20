f874c1b1a1330c00e4e96ba77cff68fe001be9f5
TheoRomanova <feodoraandriewskaya@gmail.com>
2023-06-20 17:17:34 +0300

    m

--- src/components/ShopItem/ShopItem.tsx
+++ src/components/ShopItem/ShopItem.tsx
@@ -1,6 +1,7 @@
 import "./styles.scss";
 import { ProductType } from "../../redux/products/products.slice";
 import { useEffect } from "react";
+import { getSizes } from "../../data/data";
 
 interface Props {
   product: ProductType;
@@ -17,6 +18,11 @@ export const ShopItem = ({ product }: Props) => {
         <div className="price-favorites">
           <p className="price">{product.price.current.text}</p>
           <span></span>
+          <div className="sizes">
+            {getSizes(37, 42).map((size) => (
+              <span className="size">{size}</span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
